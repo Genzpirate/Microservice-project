30fa542241c968d8185e36df16dd4657152b4cf2
Lalith <lalithreddy2004@gmail.com>
2026-07-23 20:35:13 +0530

    installed many dependencies such as react-redux,react-oauth2-code-pkce,etc and configured keycloak with  react,login will now get reddirected to keycloak

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,13 +1,40 @@
-
+import { useContext, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Button } from '@mui/material'
+import { AuthContext } from 'react-oauth2-code-pkce'
+import {BrowserRouter as Router, Navigate, Route,Routes,useLocation} from 'react-router'
+import { setCredentials } from './store/authSlice';
 
 function App() {
+
+  const {token,tokenData,logIn,logOut,isAuthenticated} = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady,setAuthReady] = useState(false);
+
   
+  useEffect(() => {
+    if(token){
+      dispatch(setCredentials({token,user: tokenData}));
+      setAuthReady(true);
+    }
+  },[token,tokenData,dispatch]);
+
 
   return (
-    <>
-      
-    </>
+    <Router>
+      {!token ? ( 
+      <Button variant="contained"
+                onClick={() => {
+                  logIn();
+                }}>login</Button>
+                ) :(
+                  <div>
+                    <pre>{JSON.stringify(tokenData,null,2)}</pre>
+                  </div>
+                )}
+    </Router>
   )
 }
 
 export default App
+ //color="#dc004e"

64a23a1c14396bbef0107506efd3488ce5de6c4d
Lalith <lalithreddy2004@gmail.com>
2026-07-24 17:33:26 +0530

    UI for adding activity is completed! next wee have to integrate the adding activity with backend using axios

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import { useContext, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { Button } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import { AuthContext } from 'react-oauth2-code-pkce'
 import {BrowserRouter as Router, Navigate, Route,Routes,useLocation} from 'react-router'
 import { setCredentials } from './store/authSlice';
+import ActivityForm from './components/ActivityForm'
+import ActivityDetail from './components/ActivityDetail'
+import ActivityList from './components/ActivityList'
 
+const ActivitiesPage = () =>{
+  return (
+  <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+    
+      <ActivityForm onActivitiesAdded = {() => window.location.reload() }/>
+      <ActivityList />
+    </Box>
+  )
+}
 function App() {
 
   const {token,tokenData,logIn,logOut,isAuthenticated} = useContext(AuthContext);
@@ -28,9 +40,20 @@ function App() {
                   logIn();
                 }}>login</Button>
                 ) :(
-                  <div>
-                    <pre>{JSON.stringify(tokenData,null,2)}</pre>
-                  </div>
+                  // <div>
+                  //   <pre>{JSON.stringify(tokenData,null,2)}</pre>
+                  //    <pre>{JSON.stringify(token,null,2)}</pre>
+
+                  // </div>
+
+                  <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
+                    <Routes>
+                      <Route path="/activities" element={<ActivitiesPage />}/>
+                      <Route path="/activities/:id" element={<ActivityDetail />}/>
+                      <Route path="/" element={token ? <Navigate to="/activities" replace/> : <div>Welcome. Please login!</div>}/>
+
+                    </Routes>
+                  </Box>
                 )}
     </Router>
   )
